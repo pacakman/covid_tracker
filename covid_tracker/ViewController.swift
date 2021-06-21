af2c7dbf09c6e9f01939274ab88493fa1c87bac6
Idris <idris@amartha.com>
2021-06-21 22:40:59 +0700

    Initial Commit

---
+++ covid_tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  covid_tracker
+//
+//  Created by Idris on 21/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
